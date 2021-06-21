13907cfffd1b2f5e91a564e38fa4774e2c29b8cc
bramvandijk88 <vandijk@evolbio.mpg.de>
2021-06-21 10:33:26 +0200

    Updated cheater

--- examples/cheater.js
+++ examples/cheater.js
@@ -6,9 +6,9 @@ let sim;
 var A2B =1.0                    // Mutualist species A giving help to reproduce B
 var B2A =1.0                    // Mutualist species B giving help to reproduce A
 var B2C =0.8                    // Mutualist species B giving help to reproduce C ("cheater")
-var p_nothing=1.0               // Probability that nothing happens when competing for empty grid point
+var p_nothing=0.1               // Probability that nothing happens when competing for empty grid point
 var death=0.2                   // Death rate of individuals
-let mdif_interval=2
+let mdif_interval=0
 
 
 function setup()
@@ -18,16 +18,16 @@ function setup()
             title: "Mutualists and cheaters",
             description: "",
             maxtime: 100000,
-            ncol : 150,            
-            nrow : 150,		                                        // dimensions of the grid to build
+            ncol : 200,            
+            nrow : 200,		                                        // dimensions of the grid to build
             seed : 56,  
-            fps : 60,                                                // Note: FPS can only be set in fastmode
-            fastmode: true,                                                        
-            throttlefps : false,                                     // Note: FPS throttling only useful at low targetfps OR when display() is the time-limiting step  
+            fps : 60,                                               // Note: FPS can only be set in fastmode
+            fastmode: true,
+            throttlefps : false,                                    // Note: FPS throttling only useful at low targetfps OR when display() is the time-limiting step  
             wrap : [true, true],                                    // Wrap boundary [COLS, ROWS]
             scale : 2,				                                // scale of the grid (nxn pixels per grid point)            
-            statecolours: {'species':{1:"#DDDDDD",                // Sets up colours of states (here 1,2,3 = A,B,C). Can be a colour name or a hexadecimal colour. 
-                                      2:"red",                    // If your state it not defined, it won't be drawn and you'll see the grid-background colour (default: black)
+            statecolours: {'species':{1:"#DDDDDD",                  // Sets up colours of states (here 1,2,3 = A,B,C). Can be a colour name or a hexadecimal colour. 
+                                      2:"red",                      // If your state it not defined, it won't be drawn and you'll see the grid-background colour (default: black)
                                       3:"blue"},
                            'alive':{1:"#CCCCCC"}}                 
     }
@@ -36,20 +36,21 @@ function setup()
     sim = new Simulation(config)                                            // Initialise a new Simulation instance with configuration given above 
     
     sim.makeGridModel("cheater")                                            // Make a new gridmodel named cheater
-    sim.initialGrid(sim.cheater,'species',1,0.33,2,0.33,3,0.33)             // Place the three 'species' in grid points (33% a, 33% b, 33% c)            
+    sim.initialGrid(sim.cheater,'species',1,0.33,2,0.33,3,0.33)             // Place the three 'species' in grid points (33% A, 33% B, 33% C)            
 
     sim.createDisplay("cheater","species")                                  // Display the 'species' property of the cheater grid
-    sim.createDisplay("cheater","alive")                                    // Display the 'alive' property of the cheater grid
-    sim.createDisplay("cheater","species",20,20,15)                         // Display the 'species' property of a small bit of the grid
+    // sim.createDisplay("cheater","alive")                                    // Display the 'alive' property of the cheater grid
+    sim.createDisplay("cheater","species",20,20,20)                         // Display the 'species' property of a small bit of the grid
     
-    sim.cheater.nextState = function(i,j)                                 // Define the next-state function. This example is two mutualists and a cheater
+    sim.cheater.nextState = function(i,j)                                    // Define the next-state function. This example is two mutualists and a cheater
     {         
-        // let pA, pB, pC, psum              
+        // let pA, pB, pC, psum             
+        
         let state = this.grid[i][j].species;
         
         if (state==0)                               // If there is no species here
         {
-            // Count the number of species 1 (mutualist x), 2 (mutualist y), and 3 (cheater z)
+            // Count the number of species 1 (mutualist A), 2 (mutualist B), and 3 (cheater C)
             sumA= this.countMoore8(this,i,j,1,'species');  
             sumB= this.countMoore8(this,i,j,2,'species');
             sumC= this.countMoore8(this,i,j,3,'species');
@@ -72,7 +73,7 @@ function setup()
         }
         else
         {
-            if (this.rng.random()<death)
+            if (this.rng.random()<death*(1-p_nothing))
                 this.grid[i][j].species = 0
         }
         this.grid[i][j].alive = state > 0 ? 1: 0    // If state is greater than 0, than set alive to 1
@@ -83,7 +84,7 @@ function setup()
     {
         this.synchronous()
         this.plotPopsizes('species',[1,2,3]) 
-        if(this.time%mdif_interval==1) this.MargolusDiffusion()
+        if(this.time%mdif_interval==0) this.MargolusDiffusion()
         let sumA = 0
         let sumB = 0
         let sumC = 0
@@ -105,21 +106,19 @@ function setup()
             [sumA/sumB,sumB/sumC],
             ["black"],
             "My custom XY plot (X/Y vs Y/Z)", {drawPoints: true, strokeWidth:1, pointSize:2, strokePattern: [2,2]})        
-        if (this.time%100==0)       // Otherwise, just print some numbers (e.g. popsizes)
+        if (this.time%10==0)       // Otherwise, just print some numbers (e.g. popsizes)
         {
-            console.log(`Cheater at time point ${this.time}, has popsizes\t\t${ sim.cheater.getPopsizes('species',[1,2,3]) }`)    
+            sim.log(`Cheater at time point ${this.time}, has popsizes\t\t${sim.cheater.getPopsizes('species',[1,2,3])}`, "output")    
         }
     }  
-
-    
-    
-    sim.addButton("pause/continue",function() { sim.toggle_play()})            // Add a button that calls function "display" in "model"
-    sim.addButton("mix grid",function() { sim.cheater.perfectMix()})           // Add a button that calls function "perfectMix" in "model.cheater"    
-    sim.addButton("well-mix",function() { sim.toggle_mix()})                   // Add a button that calls function "perfectMix" in "model.cheater"  
+        
+    sim.addButton("pause/continue",function() {sim.toggle_play()})              // Add a button that calls function "display" in "model"
+    sim.addButton("mix grid",function() { sim.cheater.perfectMix()})            // Add a button that calls function "perfectMix" in "model.cheater"    
+    sim.addButton("well-mix",function() { sim.toggle_mix()})                    // Add a button that calls function "perfectMix" in "model.cheater"  
     sim.addSlider("A2B")
     sim.addSlider("B2A")
     sim.addSlider("B2C")
-    sim.addSlider("p_nothing")
+    sim.addSlider("p_nothing",0.00,1.00,0.01)
     sim.addSlider("death", 0.00, 1.00, 0.001)
 
     sim.start()
@@ -128,6 +127,5 @@ function setup()
 // For when cheater.js is ran from the command line
 if(typeof window == "undefined")
 {   
-
     setup()
 }
