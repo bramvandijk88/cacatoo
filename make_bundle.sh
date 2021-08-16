9bf6042116e8c2052b6710d4fccc8c1b299d4113
bramvandijk88 <vandijk@evolbio.mpg.de>
2021-08-16 12:10:10 +0200

    Added TE coevolution example

--- make_bundle.sh
+++ make_bundle.sh
@@ -23,4 +23,5 @@ cp examples/03_expert/PoS_classes.js docs
 cp examples/03_expert/11_classes_PoS_TEs.html docs/TEs_host_coevol.html
 sed -i 's/..\/..\/dist\/cacatoo.js/.\/cacatoo.js/g' docs/TEs_host_coevol.html
 sed -i 's/..\/..\/lib\/all.js/.\/all.js/g' docs/TEs_host_coevol.html
+sed -i 's/..\/..\/style\/cacatoo.css/.\/cacatoo.css/g' docs/TEs_host_coevol.html
 
