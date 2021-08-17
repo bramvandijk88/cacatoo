9ad519c01bc0a86785ba27333ad8f336c80aa4d9
bramvandijk88 <vandijk@evolbio.mpg.de>
2021-08-17 17:23:22 +0200

    Updated TE model for revision

--- dist/cacatoo.js
+++ dist/cacatoo.js
@@ -1582,10 +1582,11 @@ class Simulation
             let meter = new FPSMeter({show:'ms',left:"auto", top:"45px",right:"50px",graph:1,history:20, smoothing: 30});
 
             document.title = `Cacatoo - ${this.config.title}`;
-            document.getElementById("footer").innerHTML = `<a target="blank" href="https://bramvandijk88.github.io/cacatoo/"><img class="logos" src="/images/elephant_cacatoo_small.png"></a>`;
-            document.getElementById("footer").innerHTML += `<a target="blank" href="https://github.com/bramvandijk88/cacatoo"><img class="logos" style="padding-top:32px;" src="/images/gh.png"></a></img>`;
-            document.getElementById("header").innerHTML = `<h2>Cacatoo - ${this.config.title}</h2><font size=2>${this.config.description}</font size>`;
-            document.getElementById("footer").innerHTML += "<h2>Cacatoo is a toolbox to explore individual-based models straight from your webbrowser. It is still in development. Feedback <a href=\"https://www.bramvandijk.com/contact\">very welcome.</a></h2>";
+            
+            if(document.getElementById("footer") != null) document.getElementById("footer").innerHTML = `<a target="blank" href="https://bramvandijk88.github.io/cacatoo/"><img class="logos" src="/images/elephant_cacatoo_small.png"></a>`;
+            if(document.getElementById("footer") != null)document.getElementById("footer").innerHTML += `<a target="blank" href="https://github.com/bramvandijk88/cacatoo"><img class="logos" style="padding-top:32px;" src="/images/gh.png"></a></img>`;
+            if(document.getElementById("header") != null)document.getElementById("header").innerHTML = `<h2>Cacatoo - ${this.config.title}</h2><font size=2>${this.config.description}</font size>`;
+            if(document.getElementById("footer") != null)document.getElementById("footer").innerHTML += "<h2>Cacatoo is a toolbox to explore individual-based models straight from your webbrowser. It is still in development. Feedback <a href=\"https://www.bramvandijk.com/contact\">very welcome.</a></h2>";
             let simStartTime = performance.now();
       
             async function animate()
