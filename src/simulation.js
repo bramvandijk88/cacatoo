26543cb72e0123eab54f2272be857ed041542a43
Bram van Dijk <bramvandijk88@gmail.com>
2024-11-06 09:22:09 +0100

    Fixed touch screen interactions

--- src/simulation.js
+++ src/simulation.js
@@ -255,7 +255,7 @@ class Simulation {
         canvas.elem.addEventListener('touchstart', (e) => { flockmodel.mouseDown = true})
 
         canvas.elem.addEventListener('mouseup', (e) => { flockmodel.mouseDown = false })
-        canvas.elem.addEventListener('touchend', (e) => { flockmodel.mouseDown = false})
+        canvas.elem.addEventListener('touchend', (e) => { flockmodel.mouseDown = false; flockmodel.mousecoords = {x:-1000,y:-1000}})
         canvas.elem.addEventListener('mouseout', (e) => { flockmodel.mousecoords = {x:-1000,y:-1000}})
         cnv.bgcolour = this.config.bgcolour || 'black'
         cnv.display = cnv.displayflock                
