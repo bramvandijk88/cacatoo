d8f5343965980344284ae7c7ace051b5d4bb1e7d
Bram van Dijk <bramvandijk88@gmail.com>
2025-12-12 09:51:15 +0100

    Updated boid grid point logic

--- dist/cacatoo.js
+++ dist/cacatoo.js
@@ -1633,7 +1633,8 @@ class Flockmodel {
         this.init_velocity = config.init_velocity || 0.1;
         this.rng = rng;
         this.random = () => { return this.rng.random()};
-        this.randomInt = (a,b) => { return this.rng.randomInt(a,b)};                
+        this.randomInt = (a,b) => { return this.rng.randomInt(a,b)};        
+        
         this.wrap = config.wrap || [true, true];
         
         this.wrapreflect = 1;
@@ -2387,16 +2388,19 @@ class Flockmodel {
     }
 
     // Returns the grid point corresponding to the boid's position. Returns 'null' if the boid is out of bounds
-    getGridpoint(i,gridmodel){
+    getBoidGridpoint(i,gridmodel){
         let x = Math.floor(i.position.x);
         let y = Math.floor(i.position.y);
         if(x >= 0 && x < gridmodel.nc && y >= 0 && y < gridmodel.nr) {
+            gridmodel.grid[x][y].x = x;
+            gridmodel.grid[x][y].y = y;
             return(gridmodel.grid[x][y])
         }
         else {
             return undefined
         }
     }
+    getGridpoint = this.getBoidGridpoint
 
     // TODO UITLEG
     getNearbyGridpoints(boid,gridmodel,radius){
