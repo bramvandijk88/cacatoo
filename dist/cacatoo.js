41107902e1ba7d0f8c7351f0f2c37280c35e26d0
Bram van Dijk <bramvandijk88@gmail.com>
2026-02-20 15:42:42 +0100

    Added cockatiel and aapje

--- dist/cacatoo.js
+++ dist/cacatoo.js
@@ -4889,6 +4889,10 @@ class Simulation {
             if (callback) callback();
         };
 
+        img.onerror = function() {
+            console.error("Failed to load the image: " + imageSource);
+            if (errorCallback) errorCallback();
+        };
         // Handle local files (for local use)
         if (imageSource instanceof File) {
             const reader = new FileReader();
